4c39349edff0c4aad01e8b0ba7711c18af4b0dd4
noirvin <vin.sp97@googlemail.com>
2019-09-26 21:29:59 -0700

    almost done with page 2

--- superheroes.py
+++ superheroes.py
@@ -20,8 +20,102 @@ class Ability:
       # Hint: The constructor initializes the maximum attack value.
       return random_attack_value
 
+
+class Armor:
+    def __init__(self, name, max_block):
+        '''Instantiate instance properties.
+            name: String
+            max_block: Integer
+        '''
+        # TODO: Create instance variables for the values passed in.
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        ''' Return a random value between 0 and the initialized max_block strength. '''
+
+        return random.randint( 0 , max_block )
+
+
+class Hero:
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+          abilities: List
+          armors: List
+          name: String
+          starting_health: Integer
+          current_health: Integer
+        '''
+        # TODO: Initialize instance variables values as instance variables
+        self.abilities = []
+        self.armors = []
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+        # (Some of these values are passed in above,
+        # others will need to be set at a starting value)
+        # abilities and armors are lists that will contain objects that we can use
+
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+        # TODO: Add ability object to abilities:List
+        self.abilities.append(ability)
+
+    def attack(self):
+        sum = 0
+        '''Calculate the total damage from all ability attacks.
+        return: total:Int
+        '''
+        # TODO: This method should run Ability.attack() on every ability
+        for ability in self.abilities:
+
+            # in self.abilities and returns the total as an integer.
+
+            sum += Ability.attack(ability)
+        return sum
+    def add_armor(self, armor):
+      '''Add armor to self.armors
+        Armor: Armor Object
+      '''
+      # TODO: Add armor object that is passed in to `self.armors`
+      for armor in self.armors:
+
+          self.armors.append(armor)
+
+
+    def defend(self, damage_amt):
+      sum = 0
+      '''Runs `block` method on each armor.
+          Returns sum of all blocks
+      '''
+      # TODO: This method should run the block method on each armor in self.armors
+      for armor in self.armors:
+
+         sum += armor.Armor.block(damage_amt)
+      return sum
+    def take_damage(self, damage_amt):
+      '''Updates self.current_health to reflect the damage minus the defense.
+      '''
+      # TODO: Create a method that updates self.current_health to the current
+      # minus the the amount returned from calling self.defend(damage).
+      self.current_health = self.current_health - (self.defend(damage_amt))
+
+
+    def is_alive(self):
+      '''Return True or False depending on whether the hero is alive or not.
+      '''
+      # TODO: Check whether the hero is alive and return true or false
+
+      pass
+
+
+
 if __name__ == "__main__":
 
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
+    # If you run this file from the terminal
+    # this block of code is executed.
+    hero = Hero("Grace Hopper", 200)
+    shield = Armor("Shield", 50)
+    hero.add_armor(shield)
+    hero.take_damage(50)
+    print(hero.current_health)
